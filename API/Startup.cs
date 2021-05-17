3fdf989128cd1933318a43b8572c86a22f61c320
Gaurav keshri <gk.net.213@outlook.com>
2021-05-17 22:31:36 +0530

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 namespace API
 {
@@ -30,17 +36,17 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+           
             services.AddControllers();
-
-            services.AddDbContext<DataContext>(options =>
-        options.UseSqlServer(_config.GetConnectionString("DatingDatabase")));
+            services.AddApplicationServices(_config);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
 
             services.AddCors();
+            services. AddIdentityServices(_config);
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,8 +63,11 @@ namespace API
 
             app.UseRouting();
 
+           
+
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
